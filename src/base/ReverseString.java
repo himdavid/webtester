b81e778fae39448e964a954060851c4dfc46eae9
himdavid <exta02@gmail.com>
2016-08-29 21:19:48 -0700

    Created a class to reverse a string

---
+++ src/base/ReverseString.java
@@ -0,0 +1,24 @@
+package base;
+
+public class ReverseString {
+	
+	public ReverseString(){
+		
+	}
+	
+	public String reverseStr(String str){
+		String reverse = "";
+		for(int i = str.length() - 1; i>=0; i--) {
+			reverse = reverse + str.charAt(i);
+		}
+		
+		return reverse;
+	}
+
+	public static void main(String[] args) {
+		ReverseString rs = new ReverseString();
+		System.out.println(rs.reverseStr("HeLLo"));
+
+	}
+
+}
